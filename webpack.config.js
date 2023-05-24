35f571f07d67b95d00317f1a6f281f358b7e2a5a
iddrismystic <abdulwahabiddris08@gmail.com>
2023-05-24 23:44:15 +0100

    i

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,9 @@
 const path = require("path");
-    const fileName = "List"
+
+const type = "js"
+
+if(type == "component"){
+    const fileName = "Starter"
     const extendFolder = ""
     module.exports = {
         mode:"production",
@@ -30,3 +34,37 @@ const path = require("path");
         }
     }
 
+
+}else if (type === "js"){
+    const fileName = "Fun"
+    module.exports = {
+        mode:"production",
+        entry: `./../Funcss/js/${fileName}.jsx`,
+        output: {
+            path: path.resolve("js"),
+            filename: `${fileName}.jsx`,
+            libraryTarget:"commonjs2"
+        },
+        module:{
+            rules:[
+                {
+                loader: 'babel-loader',
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                options: {
+                    presets: ['@babel/preset-env', '@babel/preset-react']
+                  }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+              }
+        ]
+        },
+        externals:{
+            react:"react"
+        }
+    }
+
+
+}
